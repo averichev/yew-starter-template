b05d2a7dd478694d6cf876ed14a4ef9e74e2133e
averichev <averichev@yandex.ru>
2023-12-06 16:18:11 +0300

    style: Format

--- src/views/main_view.rs
+++ src/views/main_view.rs
@@ -1,17 +1,17 @@
-use yew::{Component, Context, Html, html};
+use yew::{html, Component, Context, Html};
 
-pub struct MainView{}
+pub struct MainView {}
 
 impl Component for MainView {
     type Message = ();
     type Properties = ();
 
     fn create(_ctx: &Context<Self>) -> Self {
-        MainView{}
+        MainView {}
     }
 
     fn view(&self, _ctx: &Context<Self>) -> Html {
-        html!{
+        html! {
             <>
             <h1>{"Hello world!"}</h1>
             <img src={"img/logo.svg"} />
